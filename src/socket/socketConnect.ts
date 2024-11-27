125a30979bef853092563b5ff88e02c606ba1dd5
Aswer3570 <fedreg2012@yandex.ru>
2024-11-27 15:24:40 +0400

    add ngrok proxy

--- src/socket/socketConnect.ts
+++ src/socket/socketConnect.ts
@@ -1,7 +1,8 @@
 import { io } from 'socket.io-client'
 
 const URL = import.meta.env.PROD ? import.meta.env.VITE_BACKEND_URL_ADDRESS : import.meta.env.VITE_BACKEND_LOCAL_ADDRESS
-const PATH = import.meta.env.PROD ? '/api/socket.io' : ''
+// const PATH = import.meta.env.PROD ? '/api/socket.io' : ''
+const PATH = import.meta.env.PROD ? 'https://967e-46-49-42-76.ngrok-free.app' : ''
 
 const socket = io(URL, {
 	path: PATH,
