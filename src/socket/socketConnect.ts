bf83dca630adfcf1baeed2ae966d6b4767bc8b74
Aswer3570 <fedreg2012@yandex.ru>
2024-11-27 15:41:39 +0400

    add ngrok proxy

--- src/socket/socketConnect.ts
+++ src/socket/socketConnect.ts
@@ -1,12 +1,15 @@
 import { io } from 'socket.io-client'
 
 const URL = import.meta.env.PROD ? import.meta.env.VITE_BACKEND_URL_ADDRESS : import.meta.env.VITE_BACKEND_LOCAL_ADDRESS
-// const PATH = import.meta.env.PROD ? '/api/socket.io' : ''
-const PATH = import.meta.env.PROD ? 'https://967e-46-49-42-76.ngrok-free.app' : ''
+const PATH = import.meta.env.PROD ? '/api/socket.io' : ''
+// const PATH = import.meta.env.PROD ? 'https://1b7a-46-49-42-76.ngrok-free.app' : 'https://1b7a-46-49-42-76.ngrok-free.app'
 
 const socket = io(URL, {
 	path: PATH,
-	reconnection: true
+	reconnection: true,
+	extraHeaders: {
+		'ngrok-skip-browser-warning': 'true'
+	}
 })
 
 export default socket
