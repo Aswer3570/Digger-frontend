3dcbe71ff4a1d1e6e542f121886e92953e78ecc0
Aswer3570 <fedreg2012@yandex.ru>
2024-11-28 04:10:15 +0400

    change proxy url

--- src/socket/socketConnect.ts
+++ src/socket/socketConnect.ts
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client'
 
 // const URL = import.meta.env.PROD ? import.meta.env.VITE_BACKEND_URL_ADDRESS : import.meta.env.VITE_BACKEND_LOCAL_ADDRESS
-const URL = import.meta.env.PROD ? 'https://1b7a-46-49-42-76.ngrok-free.app' : '127.0.0.1:8080'
+const URL = import.meta.env.PROD ? 'https://b4be-46-49-42-76.ngrok-free.app' : '127.0.0.1:8080'
 // const PATH = import.meta.env.PROD ? '/api/socket.io' : ''
 const PATH = import.meta.env.PROD ? '' : ''
 // const PATH = import.meta.env.PROD ? 'https://1b7a-46-49-42-76.ngrok-free.app' : 'https://1b7a-46-49-42-76.ngrok-free.app'
